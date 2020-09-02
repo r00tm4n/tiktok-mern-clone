318ca0cb69ad73ead11d02db993bf9a79f10e7b1
n3n3n0 <ernestonelitositoe@gmail.com>
2020-09-02 23:26:18 +0200

    finilized UI components

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ function App() {
       {/* app container */}
 
       <div className="app__videos">
-        <Video />
-        <Video />
+        <Video url="https://v77.tiktokcdn.com/9a368f1ca4b3f743f19445639f1debcb/5f504001/video/tos/useast2a/tos-useast2a-ve-0068c003/a7aaa39f62ee43d987c617654b0873fd/?a=1233&br=1998&bt=999&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200902185929010189073165272004B2&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M292bmRtZ3dvdTMzODczM0ApNzxmOmg5aDw7NztpPDk5ZWduMzRucWpkNmpfLS0tMTZzcy40LzUyMTBfMTJeNDYuNTE6Yw%3D%3D&vl=&vr="
+        channel={"r00tm4n"} descritpion={"Long life learner"} song={"Who that cap fit"} likes={111} shares={333} messages={300}
+        />
+        <Video url="https://v77.tiktokcdn.com/9a368f1ca4b3f743f19445639f1debcb/5f504001/video/tos/useast2a/tos-useast2a-ve-0068c003/a7aaa39f62ee43d987c617654b0873fd/?a=1233&br=1998&bt=999&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200902185929010189073165272004B2&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M292bmRtZ3dvdTMzODczM0ApNzxmOmg5aDw7NztpPDk5ZWduMzRucWpkNmpfLS0tMTZzcy40LzUyMTBfMTJeNDYuNTE6Yw%3D%3D&vl=&vr="
+        channel={"r00tm4n"} descritpion={"Long life learner"} song={"Who that cap fit"} likes={111} shares={333} messages={300}
+        />
       </div>
       
         {/* videos */}
--- src/Video.js
+++ src/Video.js
@@ -1,8 +1,9 @@
 import React,{useRef, useState} from 'react';
 import "./Video.css";
 import VideoFooter from './VideoFooter';
+import VideoSidebar from './VideoSidebar';
 
-function Video() {
+function Video({url, channel, description, song, likes, shares, messages}) {
     const [playing, setplaying] = useState(false);
     const videoRef = useRef(null);
 
@@ -23,10 +24,10 @@ function Video() {
                 className="video__player"
                 loop
                 ref={videoRef}
-                src="https://v77.tiktokcdn.com/9a368f1ca4b3f743f19445639f1debcb/5f504001/video/tos/useast2a/tos-useast2a-ve-0068c003/a7aaa39f62ee43d987c617654b0873fd/?a=1233&br=1998&bt=999&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200902185929010189073165272004B2&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M292bmRtZ3dvdTMzODczM0ApNzxmOmg5aDw7NztpPDk5ZWduMzRucWpkNmpfLS0tMTZzcy40LzUyMTBfMTJeNDYuNTE6Yw%3D%3D&vl=&vr=" />
+                src={url} />
             
-            <VideoFooter />
-            {/* VideoFooter */}
+            <VideoFooter channel={channel} descritpion={description} song={song}/>
+            <VideoSidebar likes={likes} shares={shares} messages={messages} />
             {/* VideoSidebar */}
         </div>
     )
